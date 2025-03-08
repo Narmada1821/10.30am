d9d5cd7d2a2aad2265d8340cfa2c05c20dfe1d24
EC2 Default User <ec2-user@ip-172-31-13-110.ap-south-2.compute.internal>
2025-03-08 12:46:03 +0000

    4 added and 1 removed

--- sbi.py
+++ sbi.py
@@ -1,3 +1,6 @@
-git add 1
 git add . -- adding all the files (files available in working area)
 git status – to check the file status
+git init – initializing git repository ( files available in working area)
+touch – create a no of files
+touch 1 2 3
+ls – no of files available in that directory
