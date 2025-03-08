c8083fa8beadcd11e7c51cf05b158da79bbf2b71
Narmada1821 <nandurinarmada1821@gmail.com>
2025-03-08 19:16:07 +0530

    added 2 git operations

--- sbi.py
+++ sbi.py
@@ -4,3 +4,7 @@ git init – initializing git repository ( files available in working area)
 touch – create a no of files
 touch 1 2 3
 ls – no of files available in that directory
+mkdir – Make a directory
+mkdir param
+cd – change directory
+cd param
